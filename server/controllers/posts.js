6f9bd48e4d4b698bc5b51b71a425af1a592cb4ef
Kush Darji <kushdarji11@gmail.com>
2022-03-12 16:03:59 -0500

    day 2 changes connection esatblished with server

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -14,12 +14,12 @@ export const getPosts = async (req, res) => {
 export const createPost = async (req,res) => {
     const body = req.body;
 
-    const newPost = new PostMessage(post);
+    const newPost = new PostMessage(body);
     
     try {
-        await newPost.save();
+        const pp=await newPost.save();
 
-        res.status(201).json(newPost);
+        res.status(201).json(pp);
     } catch (error) {
         res.status(409).json({ message: error.message });
     }
